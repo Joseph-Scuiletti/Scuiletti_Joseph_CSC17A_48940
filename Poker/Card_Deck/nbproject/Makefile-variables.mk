27230d51095c0b41a78c76c8d64f8c62ad51e861
Joseph-Scuiletti <joseph.scuiletti@gmail.com>
2015-10-26 10:19:04 -0700

    Signed-off-by: Joseph-Scuiletti <joseph.scuiletti@gmail.com>

---
+++ Poker/Card_Deck/nbproject/Makefile-variables.mk
@@ -0,0 +1,35 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+CND_BUILDDIR=build
+CND_DISTDIR=dist
+# Debug configuration
+CND_PLATFORM_Debug=Cygwin_4.x-Windows
+CND_ARTIFACT_DIR_Debug=dist/Debug/Cygwin_4.x-Windows
+CND_ARTIFACT_NAME_Debug=card_deck
+CND_ARTIFACT_PATH_Debug=dist/Debug/Cygwin_4.x-Windows/card_deck
+CND_PACKAGE_DIR_Debug=dist/Debug/Cygwin_4.x-Windows/package
+CND_PACKAGE_NAME_Debug=carddeck.tar
+CND_PACKAGE_PATH_Debug=dist/Debug/Cygwin_4.x-Windows/package/carddeck.tar
+# Release configuration
+CND_PLATFORM_Release=Cygwin_4.x-Windows
+CND_ARTIFACT_DIR_Release=dist/Release/Cygwin_4.x-Windows
+CND_ARTIFACT_NAME_Release=card_deck
+CND_ARTIFACT_PATH_Release=dist/Release/Cygwin_4.x-Windows/card_deck
+CND_PACKAGE_DIR_Release=dist/Release/Cygwin_4.x-Windows/package
+CND_PACKAGE_NAME_Release=carddeck.tar
+CND_PACKAGE_PATH_Release=dist/Release/Cygwin_4.x-Windows/package/carddeck.tar
+#
+# include compiler specific variables
+#
+# dmake command
+ROOT:sh = test -f nbproject/private/Makefile-variables.mk || \
+	(mkdir -p nbproject/private && touch nbproject/private/Makefile-variables.mk)
+#
+# gmake command
+.PHONY: $(shell test -f nbproject/private/Makefile-variables.mk || (mkdir -p nbproject/private && touch nbproject/private/Makefile-variables.mk))
+#
+include nbproject/private/Makefile-variables.mk
